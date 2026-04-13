e543cf3132e9310a8db1d95a48630c724a59bcb4
DonaldKisaka <davidkisaka4@gmail.com>
2026-04-13 19:30:12 +0300

    Initial commit

---
+++ invoice-processor/src/test/java/com/donald/invoice_processor/InvoiceProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donald.invoice_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
